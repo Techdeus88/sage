1aea0fa7d62b440584ef45a0c40f45f0d66a741d
Techdeus <techdeusms@gmail.com>
2025-11-18 22:45:42 -0800

    more debug statements

--- lua/sage/init.lua
+++ lua/sage/init.lua
@@ -17,23 +17,28 @@ local M = {}
 function M.setup(opts)
     print("Setting up")
     print(vim.inspect(opts))
-    local config = require("sage.base.config")
-    local dashboard = require("sage.ui.dashboard")
-    local manager = require("sage.manager")
+    local _, config = pcall(require, "sage.base.config")
+    print(vim.inspect(config))
+    local _, dashboard = pcall(require, "sage.ui.dashboard")
+    print(vim.inspect(dashboard))
+    local _, manager = pcall(require, "sage.manager")
+    print(vim.inspect(manager))
 
     local merged_opts = vim.tbl_deep_extend("force", config, opts)
 
     merged_opts.sage = {
         start = vim.loop.hrtime(),
     }
+    print(vim.inspect(merged_opts))
 
     local c_ok, create_command = pcall(require, "sage.base.command")
     if not c_ok then
+        print('no command')
         return
     end
     create_command.setup(merged_opts.sage.start)
     create_command.run()
-
+    print('command ran')
     -- Call init safely
     local d_ok, _ = pcall(function()
         dashboard:init(merged_opts.dashboard)
