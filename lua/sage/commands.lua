f7136ab0e1bf226978794f2a45f5597768979678
Techdeus <techdeusms@gmail.com>
2025-11-26 14:33:42 -0800

    Added commands, config and made them globally accessible

---
+++ lua/sage/commands.lua
@@ -0,0 +1,106 @@
+---@private
+---@return Sage.Spec[], string[]
+local function get_specs_and_names()
+	local config = require("sage.config")
+	local plugin_fpaths = vim.fn.glob(config.opts.config_path .. config.opts.plugins_rpath .. "*.lua", true, true) ---@type string[]
+	local specs, names = {}, {} ---@type UnPack.Spec[], string[]
+
+	for _, plugin_fpath in ipairs(plugin_fpaths) do
+		local plugin_name = vim.fn.fnamemodify(plugin_fpath, ":t:r")
+		local success, spec = pcall(require, "plugins." .. plugin_name) ---@type boolean, UnPack.Spec
+
+		if not success then
+			vim.schedule(function()
+				vim.notify(("Failed to load plugin spec for %s"):format(plugin_name), vim.log.levels.ERROR)
+			end)
+		elseif type(spec) ~= "table" then
+			vim.schedule(function()
+				vim.notify(("Invalid spec for %s, not a table"):format(plugin_name), vim.log.levels.ERROR)
+			end)
+		else
+			if spec.depends and type(spec.depends) == "table" then
+				for _, dep in ipairs(spec.depends) do
+					if dep.src and type(dep.src) == "string" then
+						specs[#specs + 1] = dep
+						names[#names + 1] = vim.fn.fnamemodify(dep.src, ":t")
+					else
+						vim.schedule(function()
+							vim.notify(
+								("Invalid dependency for %s, missing src"):format(plugin_name),
+								vim.log.levels.ERROR
+							)
+						end)
+					end
+				end
+			end
+			if spec.src and type(spec.src) == "string" then
+				specs[#specs + 1] = spec
+				names[#names + 1] = vim.fn.fnamemodify(spec.src, ":t")
+			else
+				vim.schedule(function()
+					vim.notify(("Invalid spec for %s, missing src"):format(plugin_name), vim.log.levels.ERROR)
+				end)
+			end
+		end
+	end
+
+	return specs, names
+end
+
+---@private
+---@return string[]
+local function get_package_names()
+	local config = require("sage.config")
+	local package_fpaths = vim.fn.glob(config.opts.data_path .. config.opts.packages_rpath .. "*/", false, true) ---@type string[]
+	local package_names = {} ---@type string[]
+
+	for _, package_fpath in ipairs(package_fpaths) do
+		local package_name = vim.fn.fnamemodify(package_fpath:sub(1, -2), ":t")
+
+		package_names[#package_names + 1] = package_name
+	end
+
+	return package_names
+end
+
+local function handle_build(spec)
+    if type(spec.src) ~= "string"
+        or type(spec.data) ~= "table"
+        or type(spec.data.build) ~= "string"
+        or spec.data.build:is_empty_or_whitespace()
+    then
+        return
+    end
+
+    local config = require("sage.config")
+    local package_name = vim.fn.fnamemodify(spec.src, ":t")
+    local package_fpath = config.opts.data_path .. config.opts.packages_rpath.. package_name
+    local stat = vim.uv.fs_stat(package_fpath)
+    if not stat or stat.type ~= "directory" then
+        return
+    end
+
+    vim.schedule(function()
+        vim.notify(("Building %s..."):format(package_name), vim.log.levels.WARN)
+        local response = vim.system(vim.split(spec.data.build, " "), { cwd = package_fpath }):wait()
+        vim.notify(
+            ("Build %s for %s"):format(response.code ~= 0 and "failed" or "successful", package_name),
+            response.code ~= 0 and vim.log.levels.ERROR or vim.log.levels.INFO
+        )
+    end)
+end
+
+local M = {}
+
+M.build = function(specs)
+    if not specs or #specs == 0 then
+        specs, _ = get_specs_and_names()
+    end
+    for _, spec in ipairs(specs) do
+        handle_build(spec)
+    end
+end
+
+return M
+
+
---
+++ lua/sage/config.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+M.opts = {
+    config_path = vim.fn.stdpath("config"),
+    data_path = vim.fn.stdpath("data"),
+    packages_rpath = "/site/pack/core/opt/",
+    sage_rpath = "/site/pack/managers/start/sage/", -- ?
+    plugins_rpath = "/lua/packs/", -- starting point ->  aka directory to load packs from
+
+    add_opts = { confirm = false }, ---@type vim.pack.keyset.add
+    update_opts = { force = true }, ---@type vim.pack.keyset.update
+
+    strategy = "vimenter", -- delay or idle  -> startegies to decide how to load later stage packs
+    check_interval = 100, -- interval time   -> How often the timer checks the idle time of the user
+    delay_time_ms = 1000, -- delay time           -> How long the timer waits before loading the later staged packs
+    idle_time_ms = 1000, -- idle time        -> How long the user is idle before loading the later staged packs
+
+    --never, manual, errors, first, smart, startup, always
+    dashboard = "smart",
+    dashboard_auto_close = 2000,
+    dashboard_threshold = 500,
+    lock_windows = true,
+    auto_focus = true,
+
+    level = "INFO",
+    max_log = 1000,
+
+    install_timeout = 60000,
+}
+
+function M.setup(opts)
+    M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+end
+
+return M
